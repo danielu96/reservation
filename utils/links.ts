0a10e05b3ee6c6467342807de61fce61b24340f7
Daniel <danielu96@wp.pl>
2024-09-03 16:07:17 +0200

    profile added

--- utils/links.ts
+++ utils/links.ts
@@ -1,10 +1,11 @@
 type NavLink = {
-    href: string;
-    label: string;
-  };
-  
-  export const links: NavLink[] = [
-    { href: '/', label: 'home' }, 
-    { href: '/bookings ', label: 'bookings' }
-   
-  ];
+  href: string;
+  label: string;
+};
+
+export const links: NavLink[] = [
+  { href: '/', label: 'home' },
+  { href: '/profile ', label: 'profile' },
+  { href: '/bookings ', label: 'bookings' }
+
+];
