78cf03f415b3d78aea09f3427086ec47ffe19678
Daniel <danielu96@wp.pl>
2024-09-03 10:27:49 +0200

    Providers added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/navbar/Navbar';
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-    <body className={inter.className}>
-      <Navbar />
-      <main className='container py-10'>{children}</main>
-    </body>
-  </html>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          <main className='container py-10'>{children}</main>
+        </Providers>
+      </body>
+    </html>
   );
 }
