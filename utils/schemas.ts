c2d5f6e5ee675fbf36b91943e4e3c99fee8b7ebe
Daniel <danielu96@wp.pl>
2024-09-04 13:59:51 +0200

    validateWithZod added

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -7,3 +7,33 @@ export const profileSchema = z.object({
     lastName: z.string(),
     username: z.string(),
 });
+export function validateWithZodSchema<T>(
+    schema: ZodSchema<T>,
+    data: unknown
+): T {
+    const result = schema.safeParse(data);
+    if (!result.success) {
+        const errors = result.error.errors.map((error) => error.message);
+
+        throw new Error(errors.join(', '));
+    }
+    return result.data;
+}
+export const imageSchema = z.object({
+    image: validateFile(),
+});
+
+function validateFile() {
+    const maxUploadSize = 1024 * 1024;
+    const acceptedFileTypes = ['image/'];
+    return z
+        .instanceof(File)
+        .refine((file) => {
+            return !file || file.size <= maxUploadSize;
+        }, `File size must be less than 1 MB`)
+        .refine((file) => {
+            return (
+                !file || acceptedFileTypes.some((type) => file.type.startsWith(type))
+            );
+        }, 'File must be an image');
+}
