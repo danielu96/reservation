ea16cbb10182ddd2525439b7b17c2ab7a7f0104c
Daniel <danielu96@wp.pl>
2024-09-11 13:46:22 +0200

    Amenities and DynamicMap added

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -6,6 +6,19 @@ import PropertyDetails from '@/components/properties/PropertyDetails';
 import UserInfo from '@/components/properties/UserInfo';
 import { Separator } from '@/components/ui/separator';
 import Description from '@/components/properties/Description';
+import Amenities from '@/components/properties/Amenities';
+import { Skeleton } from '@/components/ui/skeleton';
+import dynamic from 'next/dynamic';
+
+const DynamicMap = dynamic(
+    () => import('@/components/properties/PropertyMap'),
+    {
+        ssr: false,
+        loading: () => <Skeleton className='h-[400px] w-full' />,
+    }
+);
+
+
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
     const property = await fetchPropertyDetails(params.id);
@@ -29,6 +42,8 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
             <UserInfo profile={{ firstName, profileImage }} />
             <Separator className='mt-4' />
             <Description description={property.description} />
+            <Amenities amenities={property.amenities} />
+            <DynamicMap countryCode={property.country} />
         </section>
     );
 }
