463652b2c84fd381974d2ee9956d9d4158f48cc0
Daniel <danielu96@wp.pl>
2024-09-18 15:34:39 +0200

    isNotOwner added

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -11,6 +11,9 @@ import { Skeleton } from '@/components/ui/skeleton';
 import dynamic from 'next/dynamic';
 import SubmitReview from '@/components/reviews/SubmitReview';
 import PropertyReviews from '@/components/reviews/PropertyReviews';
+import { findExistingReview } from '@/utils/actions';
+import { auth } from '@clerk/nextjs/server';
+
 
 const DynamicMap = dynamic(
     () => import('@/components/properties/PropertyMap'),
@@ -23,14 +26,20 @@ const DynamicMap = dynamic(
 
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
+    const { userId } = auth();
+
     const property = await fetchPropertyDetails(params.id);
     if (!property) redirect('/');
+    const isNotOwner = property.profile.clerkId !== userId;
+    const reviewDoesNotExist =
+        userId && isNotOwner && !(await findExistingReview(userId, property.id));
     const { baths, bedrooms, beds, guests } = property;
     const details = { baths, bedrooms, beds, guests };
     const firstName = property.profile.firstName;
     const profileImage = property.profile.profileImage;
     return (
         <section>
+
             <BreadCrumbs name={property.name} />
             <header className='flex justify-between items-center mt-4'>
                 <h1 className='text-4xl font-bold '>{property.tagline}</h1>
@@ -46,7 +55,8 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
             <Description description={property.description} />
             <Amenities amenities={property.amenities} />
             <DynamicMap countryCode={property.country} />
-            <SubmitReview propertyId={property.id} />
+            {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
+            {/* <SubmitReview propertyId={property.id} /> */}
             <PropertyReviews propertyId={property.id} />
         </section>
     );
