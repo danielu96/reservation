840e808209af8be203af25c5b8d0af0869713bee
Daniel <danielu96@wp.pl>
2024-09-26 15:45:22 +0200

    EmptyList added

--- app/rentals/page.tsx
+++ app/rentals/page.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { fetchRentals } from '@/utils/actions'
+import EmptyList from '@/components/home/EmptyList';
 
 async function RentalsPage() {
     const rentals = await fetchRentals()
+    if (rentals.length === 0) {
+        return (
+            <EmptyList
+                heading='No rentals to display.'
+                message="create a rental."
+            />
+        );
+    }
+
     return (
         <div>
             {rentals.map((rental) => {
