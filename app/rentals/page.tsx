fb90210d82172f939f6f0aa85796df1895a84feb
Daniel <danielu96@wp.pl>
2024-09-26 17:32:10 +0200

    Table added

--- app/rentals/page.tsx
+++ app/rentals/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { fetchRentals } from '@/utils/actions'
 import EmptyList from '@/components/home/EmptyList';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
 
 async function RentalsPage() {
     const rentals = await fetchRentals()
@@ -15,12 +24,25 @@ async function RentalsPage() {
 
     return (
         <div>
-            {rentals.map((rental) => {
-                const { id: propertyId, name, price } = rental;
-                return (
-                    <div key={propertyId}>{name}= {price}</div>
-                )
-            })}
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Price</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {rentals.map((rental) => {
+                        const { id: propertyId, name, price } = rental;
+                        return (
+                            <TableRow key={propertyId}>
+                                <TableCell>{name}</TableCell>
+                                <TableCell>{price}</TableCell>
+                            </TableRow>
+                        )
+                    })}
+                </TableBody>
+            </Table>
         </div>
     )
 }
